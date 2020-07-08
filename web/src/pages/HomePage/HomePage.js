915dc02c3666c8ada28c3c5dc86b40f2f0bcb744
Zack McKenna <zackmckenna@gmail.com>
2020-07-08 13:06:11 -0400

    added navbar

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,14 @@
 import FixerCell from 'src/components/FixerCell'
+import BasicLayout from 'src/layouts/BasicLayout'
 
 const HomePage = () => {
   return (
     <div>
-      <h1>HomePage</h1>
-      <FixerCell data={'test'} />
-      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+      <BasicLayout>
+        <h1>HomePage</h1>
+        <FixerCell data={'test'} />
+        <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+      </BasicLayout>
     </div>
   )
 }
